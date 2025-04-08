eb01cff5e3b2ceff3b5a1dde83e1d55d9bb5d270
Mohd Tanzeem <mohdtanzeem169@gmail.com>
2025-04-08 12:51:07 +0530

    Delete SmartContactProject directory

--- SmartContactProject/src/main/java/com/contact/project/UserRepository/MyOrderRepoository.java
+++
@@ -1,7 +0,0 @@
-package com.contact.project.UserRepository;
-import com.contact.project.entity.MyOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface MyOrderRepoository extends JpaRepository<MyOrder,Long> 
-{
-	public MyOrder findByorderId(String orderId);
-}
--- SmartContactProject/src/main/java/com/contact/project/UserRepository/QueryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.contact.project.UserRepository;
-
-import org.springframework.data.jpa.repository.*;
-import com.contact.project.entity.Query;
-public interface QueryRepository extends JpaRepository<Query,Long>
-{
-	public Query findByid(Long id);
-}
