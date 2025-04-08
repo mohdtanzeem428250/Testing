151c3f7b9e4dc973cfb1163bbf8821315566c7cf
Mohd Tanzeem <mohdtanzeem169@gmail.com>
2025-04-08 12:53:17 +0530

    Delete springbootjpaexample directory

--- springbootjpaexample/src/main/java/com/example/boot/jpa/UserDao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.boot.jpa.UserDao;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.boot.jpa.entity.User;
-public interface UserRepository extends CrudRepository<User, Integer>
-{
-
-}
